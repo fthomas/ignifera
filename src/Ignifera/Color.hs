2edd254a04c577fbd7915f037ab908e1acf21577
Frank S. Thomas <frank@timepit.eu>
2014-04-18 10:15:52 +0200

    Add Ord instance for Color

--- src/Ignifera/Color.hs
+++ src/Ignifera/Color.hs
@@ -2,3 +2,8 @@ module Ignifera.Color where
 
 data Color = White | Black
   deriving (Eq, Read, Show)
+
+instance Ord Color where
+  compare White Black = GT
+  compare Black White = LT
+  compare _     _     = EQ
