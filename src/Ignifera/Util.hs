63b0d8fc9a68fac208c398dd6e91d7c475ed6b02
Frank S. Thomas <frank@timepit.eu>
2014-05-16 18:36:04 +0200

    Move backtrack to Util

--- src/Ignifera/Util.hs
+++ src/Ignifera/Util.hs
@@ -2,3 +2,9 @@ module Ignifera.Util where
 
 allValues :: (Bounded a, Enum a) => [a]
 allValues = [minBound ..]
+
+backtrack :: (Monad m) => (c -> m c) -> (c -> Bool) -> c -> m c
+backtrack nextCandidates accept = returnOrTraverse
+  where
+    traverse c = nextCandidates c >>= returnOrTraverse
+    returnOrTraverse c = if accept c then return c else traverse c
--- src/Ignifera/Util/test.hs
+++
@@ -1,5 +0,0 @@
---backtrack :: (Monad m) => (c -> m c) -> (c -> Bool) -> c -> m c
---backtrack nextCandidates accept firstCandidate = returnOrTraverse firstCandidate
---  where
---    traverse c = nextCandidates c >>= returnOrTraverse
---    returnOrTraverse c = if accept c then return c else traverse c
