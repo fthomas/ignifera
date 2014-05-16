8be68e428484440bb444a754b85f3ad5ddd855fd
Frank S. Thomas <frank@timepit.eu>
2014-05-16 17:34:06 +0200

    Add tryOneOf parser combinator

--- src/Ignifera/Util/PgnParser.hs
+++ src/Ignifera/Util/PgnParser.hs
@@ -3,11 +3,12 @@ module Ignifera.Util.PgnParser where
 import Text.ParserCombinators.Parsec
 
 import Ignifera.GameResult
---import Ignifera.Util.GameResult
+import Ignifera.Util.GameResult
 
 terminationMarker :: Parser GameResult
-terminationMarker =
-  (string "1-0" >> return WhiteWon) <|>
-  (string "0-1" >> return BlackWon) <|>
-  (string "1/2-1/2" >> return Draw) <|>
-  (string "*" >> return Other)
+terminationMarker = tryOneOf [(showPgnMarker r, r) | r <- allGameResults]
+
+tryOneOf :: [(String, a)] -> Parser a
+tryOneOf xs = foldr1 (<|>) (map tryOne xs)
+  where
+    tryOne (s, a) = try (string s) >> return a
--- test/Main.hs
+++ test/Main.hs
@@ -1,2 +1,8 @@
+import Ignifera.GameResult
+import Ignifera.Util.PgnParser
+import Text.ParserCombinators.Parsec
+
 main :: IO ()
-main = putStrLn "Hello"
+main = do
+  print $ parse terminationMarker "" "*"
+  putStrLn "World"
